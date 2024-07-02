4131a8a6094417f1c454bbcc411b792e2b5d70f4
fahadzulfiqar29 <110390770+fahadzulfiqar29@users.noreply.github.com>
2024-07-02 09:38:43 +0500

    Add project files.

---
+++ HMS.Entity/Accommodation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entity
+{
+    public  class Accommodation
+    {
+        public int ID { get; set; }
+        public int AccommodationPackageID { get; set; }
+
+        public AccommodationPackage AccommodationPackage { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Hotel Managment System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hotel_Managment_System.MvcApplication" Language="C#" %>
