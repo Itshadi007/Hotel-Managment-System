1de5102100426a95caa585643b18791e30ec1d68
fahadzulfiqar29 <110390770+fahadzulfiqar29@users.noreply.github.com>
2024-07-02 09:59:43 +0500

    Installed Entity FrameWork and enabled Migrations

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,22 @@
+﻿using HMS.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+        }
+        public DbSet<Accommodation> accommodations { get; set; }
+        public DbSet<AccommodationType> accommodationTypes { get; set; }
+        public DbSet<AccommodationPackage> Accommodationspackages { get; set; }
+        public DbSet<Booking> bookings { get; set; }
+
+    }
+}
