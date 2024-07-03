83e3836cf8d40412ccc93780a38806a5a8f1e150
fahadzulfiqar29 <110390770+fahadzulfiqar29@users.noreply.github.com>
2024-07-03 12:02:56 +0500

    ALL LOGIN/LOGOUT   DONE

--- HMS.Data/HMSContext.cs
+++ HMS.Data/HMSContext.cs
@@ -1,4 +1,5 @@
 ﻿using HMS.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,15 +9,18 @@ using System.Threading.Tasks;
 
 namespace HMS.Data
 {
-    public class HMSContext : DbContext
+    public class HMSContext :DbContext
     {
         public HMSContext() : base("HMSConnectionString")
         {
         }
+
+      
         public DbSet<Accommodation> accommodations { get; set; }
         public DbSet<AccommodationType> accommodationTypes { get; set; }
         public DbSet<AccommodationPackage> Accommodationspackages { get; set; }
         public DbSet<Booking> bookings { get; set; }
+      
 
     }
 }
--- Hotel Managment System/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hotel_Managment_System.MvcApplication" Language="C#" %>
