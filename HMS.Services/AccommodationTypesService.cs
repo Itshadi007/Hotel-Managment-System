5c8a9d61cc9465683c20982c40bfa10c2e444e48
fahadzulfiqar29 <110390770+fahadzulfiqar29@users.noreply.github.com>
2024-07-08 13:01:29 +0500

    Edit And Delete Functionality

--- HMS.Services/AccommodationTypesService.cs
+++ HMS.Services/AccommodationTypesService.cs
@@ -2,13 +2,14 @@
 using HMS.Entity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HMS.Services
 {
-    public  class AccommodationTypesService
+    public class AccommodationTypesService
     {
         public IEnumerable<AccommodationType> GetAllAccommodationTypes()
         {
@@ -17,13 +18,43 @@ namespace HMS.Services
             return context.accommodationTypes.AsEnumerable();
         }
 
+        public AccommodationType GetAccommodationType(int ID)
+        {
+            var context = new HMSContext();
+
+            return context.accommodationTypes.Find(ID);
+        }
         public bool SaveAccommodationType(AccommodationType accommodationType)
         {
             var context = new HMSContext();
 
             context.accommodationTypes.Add(accommodationType);
 
-            return context.SaveChanges() >0;
+            return context.SaveChanges() > 0;
+        }
+
+        public bool UpdateAccommodationType(AccommodationType accommodationType)
+        {
+            var context = new HMSContext();
+
+            context.accommodationTypes.AddOrUpdate(accommodationType);
+
+            //     context.Entry(accommodationType).State = System.Data.Entity.EntityState.Modified;
+
+            return context.SaveChanges() > 0;
+        }
+
+        public bool DeleteAccommodationType(AccommodationType accommodationType)
+        {
+            var context = new HMSContext();
+
+             //   context.accommodationTypes.Remove(accommodationType.ID);
+
+            context.Entry(accommodationType).State = System.Data.Entity.EntityState.Deleted;
+
+            //     context.Entry(accommodationType).State = System.Data.Entity.EntityState.Modified;
+
+            return context.SaveChanges() > 0;
         }
     }
 }
--- HMS.Web/Areas/Dashboard/Controllers/AccomoodationTypesController.cs
+++ HMS.Web/Areas/Dashboard/Controllers/AccomoodationTypesController.cs
@@ -12,7 +12,7 @@ namespace HMS.Web.Areas.Dashboard.Controllers
 {
     public class AccommodationTypesController : Controller
     {
-       AccommodationTypesService accommodationTypesService = new AccommodationTypesService();
+        AccommodationTypesService accommodationTypesService = new AccommodationTypesService();
         public ActionResult Index()
         {
             return View();
@@ -22,38 +22,103 @@ namespace HMS.Web.Areas.Dashboard.Controllers
         {
             AccommodationTypesListingModel model = new AccommodationTypesListingModel();
 
-         model.accommodationTypes = accommodationTypesService.GetAllAccommodationTypes();
+            model.accommodationTypes = accommodationTypesService.GetAllAccommodationTypes();
+
+
             return PartialView("_Listing", model);
         }
 
         [HttpGet]
-        public ActionResult Action()
+        public ActionResult Action(int? ID)
         {
             AccommodationTypeActionModel model = new AccommodationTypeActionModel();
-            return PartialView("_Action",model);
+            if (ID.HasValue)// trying to Edit
+            {
+                var accommodationType = accommodationTypesService.GetAccommodationType(ID.Value);
+
+                model.ID = accommodationType.ID;
+                model.Name = accommodationType.Name;
+                model.Description = accommodationType.Description;
+
+
+            }
+
+            return PartialView("_Action", model);
         }
         [HttpPost]
         public JsonResult Action(AccommodationTypeActionModel model)
         {
             JsonResult jsonResult = new JsonResult();
+            var result = false;
+            if (model.ID > 0)// Trying to Edit Record Base On ID
 
-           AccommodationType accommodationType = new AccommodationType();
-            accommodationType.Name = model.Name;
+            {
+                var accommodationType = accommodationTypesService.GetAccommodationType(model.ID);
+
+                accommodationType.Name = model.Name;
+                accommodationType.Description = model.Description;
+                result = accommodationTypesService.UpdateAccommodationType(accommodationType);
+
+            }
+            else
+
+            {
+                AccommodationType accommodationType = new AccommodationType();
+                accommodationType.Name = model.Name;
                 accommodationType.Description = model.Description;
 
 
-            var result = accommodationTypesService.SaveAccommodationType(accommodationType);
+                result = accommodationTypesService.SaveAccommodationType(accommodationType);
 
+            }
             if (result)
             {
                 jsonResult.Data = new { Success = true };
             }
             else
             {
-                jsonResult.Data = new { Success = true , Message = "Unable to add Accommodation Type" };
+                jsonResult.Data = new { Success = true, Message = "Unable to Perform Action Accommodation Type" };
             }
 
             return jsonResult;
+
         }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            AccommodationTypeActionModel model = new AccommodationTypeActionModel();
+
+            var accommodationType = accommodationTypesService.GetAccommodationType(ID);
+
+            model.ID = accommodationType.ID;
+
+            return PartialView("_Delete", model);
+        }
+
+
+        [HttpPost]
+        public JsonResult Delete(AccommodationTypeActionModel model)
+        {
+            JsonResult jsonResult = new JsonResult();
+            var result = false;
+            var accommodationType = accommodationTypesService.GetAccommodationType(model.ID);
+
+
+              result = accommodationTypesService.DeleteAccommodationType(accommodationType);
+            
+            if (result)
+            {
+                jsonResult.Data = new { Success = true };
+            }
+            else
+            {
+                jsonResult.Data = new { Success = true, Message = "Unable to Perform Action Accommodation Type" };
+            }
+
+            return jsonResult;
+
+        }
+
     }
 }
